a36546c46ea4f21b93b90a65eeb506d13fad7ee7
Julio Noboa <julionoboa26@gmail.com>
2024-09-26 22:14:35 -0400

    final touches

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,6 @@ export class AppComponent {
     { title: 'Traductor', url: '/traductor', icon: 'language'},
     { title: 'Tabla de Multiplicar', url: '/multiplicar', icon: 'grid' },
     { title: 'Experiencia Personal', url: '/experiencia', icon: 'heart' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
